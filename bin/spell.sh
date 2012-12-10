f513a05130b39b0e1b4e5ac6d9876107e473ea97
Andriy Rysin <arysin@gmail.com>
2012-12-09 23:03:43 -0500

    new words

--- bin/spell.sh
+++ bin/spell.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 SORT="count"
+DICT="uk_UA"
 
 for i in $*
 do
@@ -15,10 +16,24 @@ do
                 IGNORE_DIEPR="|[аяую][щч](ий|ого|ому|им|а|ої|ій|ою|і|их|им|ими)"
 	        ;;
 	--no-ia)
-                IGNORE_IA="|іял|[рд]ія|тер$|магнет|хемі"
+                IGNORE_IA="|іял|[рд]ія|тер$|хемі"
 	        ;;
     	--no-badmix)
 		NO_BAD_MIX="1"
+		;;
+    	--split-caps)
+		SPLIT_CAPS="1"
+		;;
+    	--filter-dict=*)
+		FILTER_DICT=${i:14}
+		;;
+	--dict=*)
+	        DICT=${i:7}
+	        ;;
+	-*)
+	        echo "Невідомий аргумент «$i»"
+		echo "$0 [--sort={a,n}] [--no-ru] [--no-diepr] [--no-ia] [--no-badmix] [--split-caps] <ім’я файлу>"
+		exit
 		;;
         *)
                 FILE=$i
@@ -29,7 +44,7 @@ done
 
 if [ "$FILE" == "" ]; then
     echo "No file specified"
-    echo "Usage: $0 [--sort=a] [--no-ru] [--no-diepr] [--no-ia] [--no-badmix] filename"
+    echo "Usage: $0 [--sort=a] [--no-ru] [--no-diepr] [--no-ia] [--no-twisters] [--no-badmix] filename"
     exit 
 fi
 
@@ -44,6 +59,8 @@ function sortt() {
 }
 
 function filter_ru() {
+    echo "Відфільтровуємо російські слова" >&2
+    
     FILE=$1
     grep -v "[-']" $FILE | hunspell -d ru_RU -G > $FILE.rus.txt
     if command -v pgrep.py 2>&1> /dev/null; then
@@ -56,8 +73,16 @@ function filter_ru() {
     fi
 }
 
+function filter_1() {
+    if [ "$FILTER_DICT" != "" ]; then
+        echo "Відфільтровуємо слова-покручі" >&2
+        hunspell -l -d $FILTER_DICT
+    else
+        cat
+    fi
+}
 
-#BASE_IGNORE="[ґҐ]|^[А-ЯІЇЄҐ]"
+
 BASE_IGNORE="[ґҐ]"
 IGNORE="($BASE_IGNORE$IGNORE_DIEPR)"
 
@@ -66,17 +91,28 @@ IGNORE="($BASE_IGNORE$IGNORE_DIEPR)"
 export LC_ALL=uk_UA.UTF-8
 
 if [ "$NO_BAD_MIX" != "1" ]; then
+    echo "Генеруємо список мішанини з латиниці та кирилиці" >&2
     cat $FILE | grep -E "[а-яіїєґё][a-z]|[a-z][а-яіїєґё]" | sed -r "s/([а-яіїєґё][a-z]|[a-z][а-яіїєґё])/_\1_/g" > $FILE.bad_mix.txt
 fi
 
+echo "Перевіряємо правопис словником $DICT" >&2
+
 cat $FILE | tr '\n' '@' | sed -r "s/-@ *//g" | tr '@' '\n' |\
 sed "s/[а-яіїєґ]+\.//gi" | sed -r "s/́//g" | sed -r "s/([’ʼ‘\`]|\\')/'/g" |\
-hunspell -d uk_UA -l |\
+hunspell -d $DICT -l |\
 grep -iE "^[а-щьюяїєґ'-]{4,}$" | grep -vE "^['-]|['-]$" |\
-grep -vE "$IGNORE" | sortt > $FILE.spelled
+grep -vE "$IGNORE" | filter_1 | sortt > $FILE.spelled
+
+OUTFILE=$FILE.spelled
 
 if [ "$IGNORE_RU" == "1" ]; then
     filter_ru $FILE.spelled
+    OUTFILE=$FILE.norus.txt
+fi
+
+if [ "$SPLIT_CAPS" == "1" ]; then
+    grep "^[А-ЯІЇЄҐ]" $OUTFILE > $OUTFILE.caps
+    grep "^[^А-ЯІЇЄҐ]" $OUTFILE > $OUTFILE.nocaps
 fi
 
 # hunspell does not handle long strings (>5K utf-8 chars) well - need to split
