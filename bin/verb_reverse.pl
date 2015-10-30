d90c824da07eda0407c59c68c83eaff88b089d65
Andriy Rysin <arysin@gmail.com>
2015-10-30 16:39:49 -0400

    вити

--- bin/verb_reverse.pl
+++ bin/verb_reverse.pl
@@ -25,7 +25,7 @@ my @lines;
 my %SFX_REV = (
     A => "B", I => "J", K => "L", M => "N",
     G => "H", C => "D", E => "F",
-    O => "P", Q => "R"
+    O => "P", Q => "R", 6 => "7"
 );
 
 # для слів, які мають і "-ся" і "-сь"
@@ -54,7 +54,7 @@ while(<>) {
     
 # Заміна груп відповідними парами та зворотні інфінітиви
 
-    if( /SFX\s[AIKMGCEOQ]\s[YN]\s[0-9]+/ ) {
+    if( /SFX\s[AIKMGCEOQ6]\s[YN]\s[0-9]+/ ) {
 	my @SFX = split /\s+/, $_;
 	my $sfx_rev = $SFX_REV{$SFX[1]};
 
@@ -81,7 +81,7 @@ while(<>) {
 	next;
     }
 
-    if( /SFX\s[AIKMGCEOQ]\s/ ) {
+    if( /SFX\s[AIKMGCEOQ6]\s/ ) {
 
 	my @LINE = split /\s+/, $_;
 	my @LINE_COMMENT = split /#/, $_;
